75950ebd5c92e6a87c965010287518d31f185203
Oscar Zambrano <oscarz.developer@gmail.com>
2018-07-21 12:41:31 -0500

    Implementing workbox

---
+++ src/service-worker.js
@@ -0,0 +1,15 @@
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+
+// Precarga la app
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerNavigationRoute('/index.html')
+
+workbox.routing.registerRoute(/^https?.*/,
+  workbox.strategies.networkFirst(), 'GET')
